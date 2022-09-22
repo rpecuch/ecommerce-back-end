26930dfcab1d6a035a9af4602c5d3a56bbe084bb
userrp <109092365+rpecuch@users.noreply.github.com>
2022-09-22 13:55:14 -0400

    tested all categories api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ const { Category, Product } = require('../../models');
 // be sure to include its associated Products
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Location.findAll({
+    const categoryData = await Category.findAll({
       include: [{ model: Product }]
     });
     res.status(200).json(categoryData);
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 // be sure to include its associated Products
 router.get('/:id', async (req, res) => {
   try {
-    const categoryData = await Location.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
 
--- server.js
+++ server.js
@@ -16,4 +16,10 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
 
+//TODO:
 //bugs: running get products makes category_id and categories show up in 2 sep places instead of together
+//running post products just results in id productid and tagid. but does add to data
+//running put products results in empty object. but does update the data
+
+//all categories routes look good
+//tags get good, post good
