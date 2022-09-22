ba8f60b45d029bf7d6b30b0bc0ad1495a13bf261
userrp <109092365+rpecuch@users.noreply.github.com>
2022-09-22 18:33:45 -0400

    fixed issue with many to many relationship so that fields in product tags do not display twice

--- models/index.js
+++ models/index.js
@@ -17,18 +17,14 @@ Product.belongsTo(Category, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
